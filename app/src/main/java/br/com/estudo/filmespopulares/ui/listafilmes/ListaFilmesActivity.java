61afe13a88a34b1932ff15bbe2813ab7ba7aabb3
Wemerson0014 <wemersonf.rangel@outlook.com>
2019-01-22 14:19:09 -0200

    Criando repositório

---
+++ app/src/main/java/br/com/estudo/filmespopulares/ui/listafilmes/ListaFilmesActivity.java
@@ -0,0 +1,62 @@
+package br.com.estudo.filmespopulares.ui.listafilmes;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.estudo.filmespopulares.R;
+import br.com.estudo.filmespopulares.data.model.Filme;
+
+
+public class ListaFilmesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_filmes);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        RecyclerView  recyclerFilmes = findViewById(R.id.recycler_filmes);
+
+        RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(this);
+
+        recyclerFilmes.setLayoutManager(linearLayoutManager);
+        recyclerFilmes.setAdapter(new ListaFilmesAdapter(criaFilmes()));
+    }
+
+    private List<Filme> criaFilmes(){
+       return Arrays.asList(
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro"),
+            new Filme("Corações de ferro")
+       );
+    };
+}
